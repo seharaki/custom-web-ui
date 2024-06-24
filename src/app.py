b93fdd67610b59eb0beec4c96b85511eb0804bb0
seharaki <seharaki@amazon.com>
2024-06-24 10:15:46 -0400

    Cleaning up styling

--- src/app.py
+++ src/app.py
@@ -99,7 +99,7 @@ else:
     # Automatic token refresh
     refresh_token_if_needed()
 
-    col1, col2 = st.columns([1, 8])
+    col1, col2 = st.columns([1, 5])
 
     with col1:
         st.write("Logged in with DeviceID: ", user_email)
