c5ccce89b30df19ed4dcf2cdb90270737d331ecd
seharaki <seharaki@amazon.com>
2024-06-23 20:10:04 -0400

    revert

--- src/app.py
+++ src/app.py
@@ -1,27 +1,18 @@
 from datetime import datetime, timedelta, timezone
+
 import jwt
-import streamlit as st
+import jwt.algorithms
+import streamlit as st  #all streamlit commands will be available through the "st" alias
 import utils
+from streamlit_feedback import streamlit_feedback
 
-UTC = timezone.utc
-
-# Safety Messaging
-safety_message = '''At W, we are committed to a safety-first mindset by following all safety policies and procedures.  The Chatbot may describe a task common to a xxx facility.  Before attempting to replicate:
- 
-·        Be certain that potential hazardous energy is controlled before breaking the plane of the machine and reaching in by obtaining exclusive control through Lockout Tagout/Energy Safe Position (ESP). 
-·        Ensure proper personal protective equipment (PPE) is employed.
-·        Consult the machine specific xx, Lockout Tagout and ESP procedures, your Supervisor or Safety Manager if you are unsure of any safety requirements of the task. 
-·        Notify any employees in the area that may be affected prior to beginning the task.
-If there are safety concerns identified involving the task, please notify your Supervisor or submit it through the Concern reporting system.'''
-
-# Title
-title = "X Virtual Operator Chatbot"
+UTC=timezone.utc
 
 # Init configuration
 utils.retrieve_config_from_agent()
 
-st.set_page_config(page_title=title)
-st.title(title)
+st.set_page_config(page_title="Amazon Q Business Custom UI") #HTML title
+st.title("Amazon Q Business Custom UI") #page title
 
 # Define a function to clear the chat history
 def clear_chat_history():
@@ -32,20 +23,24 @@ def clear_chat_history():
     st.session_state["chat_history"] = []
     st.session_state["conversationId"] = ""
     st.session_state["parentMessageId"] = ""
-    st.session_state["user_prompt"] = ""  # Initialize user prompt
+
 
 oauth2 = utils.configure_oauth_component()
 if "token" not in st.session_state:
+    # If not, show authorize button
     redirect_uri = f"https://{utils.OAUTH_CONFIG['ExternalDns']}/component/streamlit_oauth.authorize_button/index.html"
-    result = oauth2.authorize_button("Start Chatting", scope="openid email", pkce="S256", redirect_uri=redirect_uri)
+    result = oauth2.authorize_button("Connect with Cognito",scope="openid", pkce="S256", redirect_uri=redirect_uri)
     if result and "token" in result:
+        # If authorization successful, save token in session state
         st.session_state.token = result.get("token")
+        # Retrieve the Identity Center token
         st.session_state["idc_jwt_token"] = utils.get_iam_oidc_token(st.session_state.token["id_token"])
-        st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(tz=UTC) + timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
+        st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(tz=UTC) + \
+            timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
         st.rerun()
 else:
-    token = st.session_state.token
-    refresh_token = token.get("refresh_token")
+    token = st.session_state["token"]
+    refresh_token = token["refresh_token"] # saving the long lived refresh_token
     user_email = jwt.decode(token["id_token"], options={"verify_signature": False})["email"]
     if st.button("Refresh Cognito Token") :
         # If refresh token button is clicked or the token is expired, refresh the token
@@ -53,50 +48,33 @@ else:
         # Put the refresh token in the session state as it is not returned by Cognito
         token["refresh_token"] = refresh_token
         # Retrieve the Identity Center token
+
         st.session_state.token = token
         st.rerun()
 
     if "idc_jwt_token" not in st.session_state:
         st.session_state["idc_jwt_token"] = utils.get_iam_oidc_token(token["id_token"])
-        st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(UTC) + timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
+        st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(UTC) + \
+            timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
     elif st.session_state["idc_jwt_token"]["expires_at"] < datetime.now(UTC):
+        # If the Identity Center token is expired, refresh the Identity Center token
         try:
-            st.session_state["idc_jwt_token"] = utils.refresh_iam_oidc_token(st.session_state["idc_jwt_token"]["refreshToken"])
-            st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(UTC) + timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
+            st.session_state["idc_jwt_token"] = utils.refresh_iam_oidc_token(
+                st.session_state["idc_jwt_token"]["refreshToken"]
+                )
+            st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(UTC) + \
+                timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
         except Exception as e:
             st.error(f"Error refreshing Identity Center token: {e}. Please reload the page.")
-    st.warning(st.session_state["idc_jwt_token"])
-    col1, col2 = st.columns([1, 1])
+
+    col1, col2 = st.columns([1,1])
 
     with col1:
-        st.write("Logged in with DeviceID: ", user_email)
+        st.write("Welcome: ", user_email)
     with col2:
-        st.button("Clear Chat", on_click=clear_chat_history)
-    
-        # Define sample questions
-    sample_questions = [
-        "This is sample question 1",
-        "This is sample question 2",
-        "This is sample question 3"
-    ]
+        st.button("Clear Chat History", on_click=clear_chat_history)
 
-    # Track which sample questions have been clicked
-    if "clicked_samples" not in st.session_state:
-        st.session_state.clicked_samples = []
-
-    # Display sample question buttons if they haven't been clicked yet
-    if token:
-        remaining_questions = [q for q in sample_questions if q not in st.session_state.clicked_samples]
-        if remaining_questions:
-            st.markdown("<div style='font-size:16px;'>Frequently Asked Questions</div>", unsafe_allow_html=True)
-            cols = st.columns(len(remaining_questions))
-            for idx, question in enumerate(remaining_questions):
-                if cols[idx].button(question, key=question, help="Click to ask this question", use_container_width=True):
-                    st.session_state.clicked_samples.append(question)
-                    st.session_state.user_prompt = question
-                    st.session_state.messages.append({"role": "user", "content": question})
-                    st.experimental_rerun()
-
+    # Initialize the chat messages in the session state if it doesn't exist
     if "messages" not in st.session_state:
         st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
@@ -118,32 +96,28 @@ else:
     if "input" not in st.session_state:
         st.session_state.input = ""
 
-    if "user_prompt" not in st.session_state:
-        st.session_state.user_prompt = ""
 
     # Display the chat messages
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.write(message["content"])
 
-    # Handle user input from the chat input box
-    if prompt := st.chat_input(key="chat_input"):
-        st.session_state.user_prompt = prompt
+
+    # User-provided prompt
+    if prompt := st.chat_input():
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.write(prompt)
-        st.session_state["show_feedback"] = False
 
-    if st.session_state.messages and st.session_state.messages[-1]["role"] == "user":
+
+    # If the last message is from the user, generate a response from the Q_backend
+    if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
                 placeholder = st.empty()
-                response = utils.get_queue_chain(
-                    st.session_state.user_prompt,
-                    st.session_state["conversationId"],
-                    st.session_state["parentMessageId"],
-                    st.session_state["idc_jwt_token"]["idToken"]
-                )
+                response = utils.get_queue_chain(prompt,st.session_state["conversationId"],
+                                                 st.session_state["parentMessageId"],
+                                                 st.session_state["idc_jwt_token"]["idToken"])
                 if "references" in response:
                     full_response = f"""{response["answer"]}\n\n---\n{response["references"]}"""
                 else:
@@ -152,75 +126,9 @@ else:
                 st.session_state["conversationId"] = response["conversationId"]
                 st.session_state["parentMessageId"] = response["parentMessageId"]
 
-            st.session_state.messages.append({"role": "assistant", "content": full_response})
-            utils.store_message_response(
-                user_email=user_email,
-                conversation_id=st.session_state["conversationId"],
-                parent_message_id=st.session_state["parentMessageId"],
-                user_message=st.session_state.user_prompt,
-                response=full_response
-            )
-            st.session_state["show_feedback"] = True
-            st.session_state["show_feedback_success"] = False
-            st.warning(safety_message, icon="🚨")
 
-if "show_feedback" not in st.session_state:
-    st.session_state["show_feedback"] = False
-
-if st.session_state["show_feedback"]:
-    col1, col2, _ = st.columns([1, 1, 10])
-    feedback_type = None
-    if col1.button("👍", key="thumbs_up"):
-        feedback_type = "👍 Thumbs Up"
-        st.session_state["feedback_type"] = feedback_type
-        utils.store_feedback(
-            user_email=user_email,
-            conversation_id=st.session_state["conversationId"],
-            parent_message_id=st.session_state["parentMessageId"],
-            user_message=st.session_state.user_prompt,
-            feedback={"type": feedback_type}
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+        feedback = streamlit_feedback(
+            feedback_type="thumbs",
+            optional_text_label="[Optional] Please provide an explanation",
         )
-        st.session_state["show_feedback"] = False
-        st.session_state["feedback_type"] = ""
-        st.session_state["show_feedback_success"] = True
-        st.experimental_rerun()
-    if col2.button("👎", key="thumbs_down"):
-        feedback_type = "👎 Thumbs Down"
-        st.session_state["feedback_type"] = feedback_type
-
-    additional_feedback = ""
-
-    if st.session_state.get("feedback_type") == "👎 Thumbs Down":
-        feedback_reason = st.selectbox(
-            "Please select the reason for your feedback:",
-            ["Not Relevant/Off Topic", "Not Accurate", "Not Enough Information", "Other"],
-            key="feedback_selectbox"
-        )
-        if feedback_reason == "Other":
-            additional_feedback = st.text_input("Please provide additional feedback:", key="additional_feedback_input")
-
-        if st.button("Submit Feedback", key="submit_feedback_button"):
-            if feedback_reason == "Other" and not additional_feedback:
-                st.warning("Please provide additional feedback for 'Other'.")
-            else:
-                feedback_details = feedback_reason
-                if additional_feedback:
-                    feedback_details = additional_feedback
-
-                utils.store_feedback(
-                    user_email=user_email,
-                    conversation_id=st.session_state["conversationId"],
-                    parent_message_id=st.session_state["parentMessageId"],
-                    user_message=st.session_state.user_prompt,
-                    feedback={"type": st.session_state["feedback_type"], "reason": feedback_details}
-                )
-                st.session_state["show_feedback"] = False
-                st.session_state["feedback_type"] = ""
-                st.session_state["feedback_reason"] = ""
-                st.session_state["additional_feedback"] = ""
-                st.session_state["show_feedback_success"] = True
-                st.experimental_rerun()
-
-if "show_feedback_success" in st.session_state and st.session_state["show_feedback_success"]:
-    st.success("Thank you for your feedback!")
-    st.session_state["show_feedback_success"] = False
