7eaa6a80ac69c6402fa7c07e9530bddb39606b68
seharaki <seharaki@amazon.com>
2024-06-25 20:22:30 -0400

    Chaging time

--- src/app.py
+++ src/app.py
@@ -50,7 +50,7 @@ def get_remaining_session_time():
 def refresh_token_if_needed():
     if "idc_jwt_token" in st.session_state:
         remaining_time = get_remaining_session_time()
-        if remaining_time and remaining_time < timedelta(minutes=59):
+        if remaining_time and remaining_time < timedelta(minutes=58):
             try:
                 token = oauth2.refresh_token(st.session_state.token, force=True)
                 # Store refresh token if available
@@ -60,11 +60,14 @@ def refresh_token_if_needed():
                     token["refresh_token"] = st.session_state.refresh_token
                 # Retrieve the Identity Center token
                 st.session_state.token = token
-                st.session_state["idc_jwt_token"] = utils.refresh_iam_oidc_token(token["id_token"], config=config_agent)
+                st.session_state["idc_jwt_token"] = utils.get_iam_oidc_token(token["id_token"], config=config_agent)
                 st.session_state["idc_jwt_token"]["expires_at"] = datetime.now(tz=UTC) + timedelta(seconds=st.session_state["idc_jwt_token"]["expiresIn"])
             except Exception as e:
                 st.error(f"Error refreshing token: {e}. Refresh the page.")
-
+                del st.session_state["token"]
+                if "refresh_token" in st.session_state:
+                    del st.session_state["refresh_token"]
+                st.rerun()
  
 oauth2 = utils.configure_oauth_component(config_agent.OAUTH_CONFIG)
 if "token" not in st.session_state:
