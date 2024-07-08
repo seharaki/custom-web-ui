1a61556d5c2360751eab6df187d68643b4fdc97b
seharaki <seharaki@amazon.com>
2024-07-07 20:01:22 -0400

    Updating targeted stlying

--- src/app.py
+++ src/app.py
@@ -261,11 +261,11 @@ if st.session_state.show_feedback:
     st.markdown(
         """
         <style>
-        button[data-testid="thumbs_up"] {
+        [data-testid="stVerticalBlock"] button[data-testid="thumbs_up"] {
             background-color: green !important;
             font-size: 24px !important;
         }
-        button[data-testid="thumbs_down"] {
+        [data-testid="stVerticalBlock"] button[data-testid="thumbs_down"] {
             background-color: red !important;
             font-size: 24px !important;
         }
@@ -274,7 +274,7 @@ if st.session_state.show_feedback:
         unsafe_allow_html=True
     )
 
-    if col1.button("👍", key="thumbs_up", data-testid="thumbs_up"):
+    if col1.button("👍", key="thumbs_up"):
         feedback_type = "👍 Thumbs Up"
         st.session_state["feedback_type"] = feedback_type
         utils.store_feedback(
@@ -289,7 +289,7 @@ if st.session_state.show_feedback:
         st.session_state["feedback_type"] = ""
         st.session_state["show_feedback_success"] = True
         st.experimental_rerun()
-    if col2.button("👎", key="thumbs_down", data-testid="thumbs_down"):
+    if col2.button("👎", key="thumbs_down"):
         feedback_type = "👎 Thumbs Down"
         st.session_state["feedback_type"] = feedback_type
 
